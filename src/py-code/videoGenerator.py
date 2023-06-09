e1bf88c95fc3f50d0e37722974d785c4c4be8a94
qayyumayaan <96453808+qayyumayaan@users.noreply.github.com>
2023-06-09 19:57:49 -0400

    Fix color mapping

--- src/py-code/videoGenerator.py
+++ src/py-code/videoGenerator.py
@@ -2,37 +2,41 @@ import cv2
 import numpy as np
 from PIL import Image
 
-# Mapping of characters to RGB colors
+# Mapping of characters to GBR colors
 color_mapping = {
-    'D': (0, 0, 0),     # black
-    'B': (0, 0, 255),   # blue
+    'D': (50, 50, 50),  # black
+    'W': (255, 255, 255), # white
+    'R': (0, 0, 255),   # red
     'G': (0, 255, 0),   # green
-    'C': (0, 255, 255), # cyan
-    'R': (255, 0, 0),   # red
+    'B': (255, 0, 0),   # blue
+    'C': (255, 255, 0), # cyan
     'P': (128, 0, 128), # purple
-    'Y': (255, 255, 0), # yellow
-    'W': (255, 255, 255)# white
+    'Y': (0, 255, 255), # yellow
 }
+
+numPixels = 144
+height = 9
+width = 16
 
 # Initialize video writer
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
 video = cv2.VideoWriter('converted.mp4', fourcc, 30.0, (1920, 1080))
 
-with open('colors.txt', 'r') as f:
+with open('colors1.txt', 'r') as f:
     colors = f.read().replace('\n', '')
 
-for i in range(0, len(colors), 144):
+for i in range(0, len(colors), numPixels):
     frame = np.zeros((1080, 1920, 3), dtype=np.uint8)
 
-    for j in range(144):
+    for j in range(numPixels):
         if i + j >= len(colors):
             break
 
         color = color_mapping[colors[i+j]]
         
-        start_row = (j // 12) * 120
+        start_row = (j // height) * 120
         end_row = start_row + 120
-        start_col = (j % 12) * 120
+        start_col = (j % width) * 120
         end_col = start_col + 120
 
         frame[start_row:end_row, start_col:end_col] = color
@@ -41,3 +45,5 @@ for i in range(0, len(colors), 144):
 
 # Release the video writer
 video.release()
+
+print("Finished converting!")
