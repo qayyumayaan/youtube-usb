fc136a90a7cf90c9e56083a507dd510934e1510d
qayyumayaan <96453808+qayyumayaan@users.noreply.github.com>
2023-06-09 18:13:16 -0400

    Add a inverter function

--- src/py-code/main.py
+++ src/py-code/main.py
@@ -1,19 +1,38 @@
 def convert_to_binary(input_file, output_file):
-    with open(input_file, 'r') as file:
+    with open(input_file, 'rb') as file:
         content = file.read()
 
-    binary_content = ''.join(format(ord(char), 'b') for char in content)
+    binary_content = ''.join(format(byte, '08b') for byte in content)
 
     with open(output_file, 'w') as file:
         file.write(binary_content)
 
-    print("Conversion completed!")
+    print("Conversion to binary completed!")
+    
+def convert_binary_to_text(input_file, output_file):
+    # Read the binary content from the input file
+    with open(input_file, 'r') as file:
+        binary_content = file.read()
 
-input_file_path = r"C:\Users\amazi\Downloads\Digital to YouTube.txt"
-output_file_path = r"C:\Users\amazi\Documents\GitHub\youtube-usb\src\py-code\output.txt"
+    # Convert binary to characters
+    text_content = bytearray()
+    for i in range(0, len(binary_content), 8):
+        byte = binary_content[i:i+8]
+        text_content.append(int(byte, 2))
+
+    # Write the text content to the output file
+    with open(output_file, 'wb') as file:
+        file.write(text_content)
+
+    print("Conversion from binary completed!")
+
 
 # input_file_path = input("Input: ")
-
 # output_file_path = input("Output: ")
 
-convert_to_binary(input_file_path, output_file_path)
+input_file_path = r"C:\Users\amazi\Downloads\Digital to YouTube.txt"
+intermediary_form_path = r"C:\Users\amazi\Documents\GitHub\youtube-usb\src\py-code\intermediary_form.txt"
+output_file_path = r"C:\Users\amazi\Documents\GitHub\youtube-usb\src\py-code\output.txt"
+
+convert_to_binary(input_file_path, intermediary_form_path)
+convert_binary_to_text(intermediary_form_path, output_file_path)
