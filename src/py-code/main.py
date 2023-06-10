4582114cc2b90c08b5d9eecb4be7ae8803591f80
qayyumayaan <96453808+qayyumayaan@users.noreply.github.com>
2023-06-10 12:44:16 -0400

    Delete pycache

--- src/py-code/main.py
+++ src/py-code/main.py
@@ -2,16 +2,17 @@ from encodeAndDecode import Encode, Decode
 import sys
 
 # inputFilePath = r"C:\Users\amazi\Downloads\Youtube data.png"
-# outputVideoPath = r"C:\Users\amazi\Documents\GitHub\youtube-usb\src\py-code\videoMade.mp4"
+# outputVideoPath = r"C:\Users\amazi\Downloads\videoMade.mp4"
+# encode = True
 # inputVideoPath = outputVideoPath
 # outputFilePath = r"C:\Users\amazi\Documents\GitHub\youtube-usb\src\py-code\decoded.png"
 
 inputFilePath = sys.argv[1]
 outputVideoPath = sys.argv[2]
 encode = sys.argv[3]    
-print(encode)
-inputVideoPath = outputVideoPath
-outputFilePath = r"C:\Users\amazi\Documents\GitHub\youtube-usb\src\py-code\decoded.png"
+# print(encode)
+# inputVideoPath = outputVideoPath
+# outputFilePath = r"C:\Users\amazi\Documents\GitHub\youtube-usb\src\py-code\decoded.png"
 
 color_mapping = {
     'D': (50, 50, 50),  # black
